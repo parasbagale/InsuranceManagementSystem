5e3d7d7ae443b9cb24c0388775fbeb9b66194c38
SANJINA <sashakya548@gmail.com>
2023-08-11 22:46:45 -0500

    policy management module

--- src/com/insurence/main/PolicyManagementModule.java
+++ src/com/insurence/main/PolicyManagementModule.java
@@ -1,6 +1,8 @@
 package com.insurance.main;
 
 import com.insurance.model.Policy;
+
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -8,6 +10,7 @@ public class PolicyManagementModule {
 
     //create, update, delete , search policy
      static Map<String, Policy> policyMap = new HashMap<>();
+    static Scanner s= new Scanner(System.in);
 
 
      //Policy p1 = new Policy(101,"1011","Health Insurance",50000.0,50.0,"2022-10-10","2023-10-10","Active",true);
@@ -22,9 +25,7 @@ public class PolicyManagementModule {
         System.out.println("3. Delete a Policy. Please enter 3 to delete an existing policy");
         System.out.println("4. Search a Policy. Please enter 4 to search an existing policy");
     }
-
     public static void CreatePolicy(){
-        Scanner s= new Scanner(System.in);
         System.out.println("Let us create a policy for you. Please provide your details: ");
         System.out.println("Enter customerId: ");
         int customerId = s.nextInt();
@@ -32,7 +33,7 @@ public class PolicyManagementModule {
         System.out.println("Please enter a policy type (Health or Home): ");
         String pType = s.next();
 
-        if("Health".equals(pType) || "Home".equals(pType)){
+        if("Health".equalsIgnoreCase(pType) || "Home".equalsIgnoreCase(pType)){
             //add new policy for a customer
             String newPolicyId = addPolicy(customerId, pType);
             if(newPolicyId != null ){
@@ -42,7 +43,6 @@ public class PolicyManagementModule {
             System.out.println("Please enter the correct policy type.");
         }
     }
-
     public static boolean verifyCustomer(int Id){
         //get all ids of customer  Question to Deepak?
         //loop through it to find this id
@@ -51,19 +51,18 @@ public class PolicyManagementModule {
 
         return true;
     }
-
     public static String addPolicy(int customerId, String pType){
 
         double coverageAmt, premiumAmt;
         String startDate = String.valueOf(LocalDate.now());
         String endDate = String.valueOf(LocalDate.now().plusYears(1));
 
-        switch (pType){
-            case "Health":
+        switch (pType.toUpperCase()){
+            case "HEALTH":
                 coverageAmt = 50000.00;
                 premiumAmt = 500.00;
                 break;
-            case "Home":
+            case "HOME":
                 coverageAmt = 200000.00;
                 premiumAmt = 1000.00;
                 break;
@@ -80,16 +79,62 @@ public class PolicyManagementModule {
         return newPolicy.getPolicyId();
     }
 
+    public static void updatePolicy(String policyId){
+        System.out.println("Please enter policyId you want to update: ");
+        String input = s.next();
+        //check input
+        showPolicyInfo(input);
+        System.out.println("You can only update policy status! Enter Yes/No");
+        String word = s.next();
+        if(word.equalsIgnoreCase("Yes")){
+            updatePolicyStatus(policyId);
+        }
+    }
+
+    public static void showPolicyInfo(String id){
+        Policy currentPolicy = policyMap.get(id);
+        System.out.println("Your policy details: \n");
+        System.out.println("CustomerId: " + currentPolicy.getCustomerId());
+        System.out.println("Policy Type: " + currentPolicy.getPolicyType());
+        System.out.println("Coverage: " + currentPolicy.getCoverageAmount());
+        System.out.println("Monthly Premium: " + currentPolicy.getPremiumAmount());
+        System.out.println("Start date: " + currentPolicy.getStartDate());
+        System.out.println("End date: " + currentPolicy.getEndDate());
+        System.out.println("Policy status: " + currentPolicy.getStatus());
+        System.out.println("Claim status: " + currentPolicy.isClaimed());
+    }
+    public static void updatePolicyStatus(String policyId){
+        Policy currentPolicy = policyMap.get(policyId);
+        String currentStatus = currentPolicy.getStatus();
+        if("Active".equalsIgnoreCase(currentStatus)){
+            currentPolicy.setStatus("Inactive");
+            System.out.println("Policy status updated to inactive.");
+        }else{
+            currentPolicy.setStatus("Active");
+            System.out.println("Policy status updated to active.");
+        }
+    }
+    public static void DeletePolicy(String policyId){
+        Policy currentPolicy = policyMap.get(policyId);
+        policyMap.remove(policyId);
+        System.out.println(policyId + " has been successfully removed from the system.");
+    }
+    public static void SearchPolicy(String policyId){
+        boolean found = false;
+        for(Map.Entry<String, Policy> entry : policyMap.entrySet()){
+            if( entry.getKey().equals(policyId)){
+                found = true;
+                showPolicyInfo(policyId);
+                break;
+            }
+        }
+
+        if(!found){
+            System.out.println("Sorry, policy not found!");
+        }
+    }
     public static Map<String, Policy> displayData(){
         return policyMap;
     }
-
-
-
-
-
-
-
-
 
 }
